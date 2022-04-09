a11efec58730d90d0ac2905e8344056febb14048
Lucashylario <lucashyfrotend@hotmail.com>
2022-04-09 14:59:34 -0300

    estilização da página de login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+import { useState } from 'react';
 import loginImg from './assets/img-login.png';
 
 import './styles.css';
 
 function App() {
+  const [email, setEmail] = useState("") /* capturar valor do input Email*/
+  const [password, setPassword] = useState("") /* capturar valor do input Senha*/
+
   return (
     <div className="container">
       <div className="container-login">
         <div className="wrap-login">
           <form className="login-form">
             <span className="login-form-title">Bem Vindo!</span>
-            <span><img src={loginImg} alt="logo"></img></span>
+            <span className="login-form-title"><img src={loginImg} alt="logo"></img></span>
 
             <div className='wrap-input'>
-              <input type="email" />
+              <input 
+                className={email !== "" ? 'input has-value' : 'input'} /* Condição: se o email for diferente (!==) de vazio, ele ativa as classes "has-value input", se não tiver vazio (:) recebe "input" */
+                type="email" 
+                value={email} 
+                onChange={event => setEmail(event.target.value)} 
+              />
                 <span className='focus-input' data-placeholder='Email'></span> {/*  efeito degrade na linha do login*/}
             </div>
 
             <div className='wrap-input'>
-              <input type="password" />
+              <input 
+                className={email !== "" ? 'input has-value' : 'input'} /* Condição: se o email for diferente (!==) de vazio, ele ativa as classes "has-value input", se não tiver vazio (:) recebe "input" */
+                type="password" value={password} 
+                onChange={event => setPassword(event.target.value)} 
+              />
                 <span className='focus-input' data-placeholder='Password'></span> {/*  efeito degrade na linha do login*/}
             </div>
 
@@ -26,7 +39,7 @@ function App() {
             </div>
 
             <div className='text-center'>
-              <span className='text-1'>Não possui conta?</span>
+              <span className='txt-1'>Não possui conta?</span>
 
               <a className='txt-2' href='#'>Criar conta.</a>
             </div>
